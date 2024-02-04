1787b083399cef42fc918c4c84c672300c707b13
tedmoon <mtj1015@naver.com>
2024-02-04 20:47:43 +0900

    2024.02.04

--- Mini_Project20240201/app/src/main/java/kr/co/lion/mini_project20240201/activity_edit.kt
+++ Mini_Project20240201/app/src/main/java/kr/co/lion/mini_project20240201/activity_edit.kt
@@ -14,13 +14,14 @@ class activity_edit : AppCompatActivity() {
         activityEditBinding = ActivityEditBinding.inflate(layoutInflater)
         setContentView(activityEditBinding.root)
 
-        setToolbar()
         setView()
         setEvent()
     }
 
-    // 툴바 설정
-    fun setToolbar(){
+
+    // View 설정
+    fun setView(){
+        // 툴바 설정
         activityEditBinding.apply {
             toolbarEdit.apply {
                 // 타이틀
@@ -30,14 +31,11 @@ class activity_edit : AppCompatActivity() {
                 inflateMenu(R.menu.menu_edit)
 
                 // 뒤로가기
-                // TODO: 뒤로가기 버튼 만들기
+                setNavigationIcon(R.drawable.arrow_back_24px)
 
             }
         }
-    }
 
-    // View 설정
-    fun setView(){
         // 수정하고자 하는 동물의 정보를 출력
 
     }
@@ -45,6 +43,14 @@ class activity_edit : AppCompatActivity() {
     // 이벤트 설정
     fun setEvent(){
         // 완료 버튼을 누르면 수정을 완료하고 activity_report화면으로 돌아간다.
+
+        // 뒤로가기 버튼 클릭시 실행 취소하고 뒤로 돌아간다.
+        activityEditBinding.apply {
+            toolbarEdit.setNavigationOnClickListener {
+                setResult(RESULT_CANCELED)
+                finish()
+            }
+        }
 
     }
 
