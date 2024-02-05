fc1f2812ac68d1031ac6d331b101e0a3ba23cabe
tedmoon <mtj1015@naver.com>
2024-02-05 17:05:45 +0900

    git test용 주석

--- Android05_FrameLayout/app/src/main/java/kr/co/lion/android05_framelayout/MainActivity.kt
+++ Android05_FrameLayout/app/src/main/java/kr/co/lion/android05_framelayout/MainActivity.kt
@@ -7,7 +7,7 @@ import android.os.Bundle
 // 배치되는 View들이 좌측 상단에 붙어서 겹쳐져서 배치되는 View이다
 // 탭이나 자유로운 배치 등을 목적으로 사용한다.
 // 지금은 다른 뷰들을 더 많이 사용한다.
-
+// 혹시 몰라서 작성하는 github 테스트용 주석
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
