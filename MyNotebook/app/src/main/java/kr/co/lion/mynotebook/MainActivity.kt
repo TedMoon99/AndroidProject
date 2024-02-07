6fead47204e9d5782f11014572744f10027d1f0a
tedmoon <mtj1015@naver.com>
2024-02-07 15:25:26 +0900

    2024.02.07

--- MyNotebook/app/src/main/java/kr/co/lion/mynotebook/MainActivity.kt
+++ MyNotebook/app/src/main/java/kr/co/lion/mynotebook/MainActivity.kt
@@ -13,22 +13,7 @@ class MainActivity : AppCompatActivity() {
 
         activityMainBinding = ActivityMainBinding.inflate(layoutInflater)
         setContentView(activityMainBinding.root)
-        setView()
 
     }
 
-    // View 설정
-    fun setView(){
-        activityMainBinding.apply {
-            // 메뉴
-            toolbarTest.apply {
-                // 타이틀
-                title = "Toolbar in Appbar"
-                // 메뉴
-                inflateMenu(R.menu.menu_toolbar_in_appbar)
-            }
-            this.textView.text = "이 쪽부터는 Appbar Layout의 영역이 아닙니다!"
-
-        }
-    }
 }
